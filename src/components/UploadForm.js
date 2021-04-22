08d8f03d4cc39a19b8f7816400741cd81afe1978
akhand patel <akhand3108patel@gmail.com>
2021-04-22 23:57:41 +0530

    Refractor Uploads

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -6,8 +6,8 @@ import { Ring } from "react-css-spinners"
 function UploadForm() {
   const [file, setFile] = useState(null)
   const [error, setError] = useState(null)
-  const [optimizing, setOptimizing] = useState(false)
   const types = ["image/jpeg", "image/png"]
+  let optimizing = false
 
   const changeHandler = async (e) => {
     let selected = e.target.files[0]
@@ -18,10 +18,10 @@ function UploadForm() {
         useWebWorker: true,
       }
       try {
-        setOptimizing(true)
+        optimizing = true
         const compressedFile = await imageCompression(selected, options)
         setFile(compressedFile)
-        setOptimizing(false)
+        optimizing = false
         e.target.value = null
         // write your own logic
       } catch (error) {
