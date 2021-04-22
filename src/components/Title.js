52508a9b1b7c9d798b725b24294bfadf6b74ac0f
akhand patel <akhand3108patel@gmail.com>
2021-04-22 23:49:36 +0530

    Feat Added optimizing loader

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import { MdBrightness6 } from "react-icons/md"
+
 function Title() {
+  const [darkMode, setDarkMode] = useState(false)
+  const darkModeToggler = () => {
+    if (!darkMode) {
+      document.body.classList.add("dark-theme")
+      document.body.classList.remove("light-theme")
+    } else {
+      document.body.classList.add("light-theme")
+      document.body.classList.remove("dark-theme")
+    }
+    setDarkMode(!darkMode)
+  }
+
   return (
     <header>
-      <h1>Firegram</h1>
+      <h1>
+        Firegram{" "}
+        <MdBrightness6
+          style={{ cursor: "pointer", "margin-left": "10px" }}
+          onClick={darkModeToggler}
+        />
+      </h1>
       <p>Your Personal Image Gallery</p>
     </header>
   )
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,13 +1,15 @@
 import { useState } from "react"
 import ProgressBar from "./ProgressBar"
 import imageCompression from "browser-image-compression"
+import { Ring } from "react-css-spinners"
 
 function UploadForm() {
   const [file, setFile] = useState(null)
   const [error, setError] = useState(null)
+  const [optimizing, setOptimizing] = useState(false)
   const types = ["image/jpeg", "image/png"]
+
   const changeHandler = async (e) => {
-    
     let selected = e.target.files[0]
     if (selected && types.includes(selected.type)) {
       const options = {
@@ -16,11 +18,11 @@ function UploadForm() {
         useWebWorker: true,
       }
       try {
-        setError("Optimizing your image")
+        setOptimizing(true)
         const compressedFile = await imageCompression(selected, options)
         setFile(compressedFile)
-        setError("")
-        e.target.value = null;
+        setOptimizing(false)
+        e.target.value = null
         // write your own logic
       } catch (error) {
         console.log(error)
@@ -35,10 +37,14 @@ function UploadForm() {
     <form>
       <input id="file-input" type="file" onChange={changeHandler} />
       <div className="output">
-        {error && <div className="error">{error}</div>}
-        {file && (
-          <ProgressBar file={file} setFile={setFile} />
+        {optimizing && (
+          <div>
+            <p>"Optimizing your Image"</p>
+            <Ring color="#3282b8" />
+          </div>
         )}
+        {error && <div className="error">{error}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
     </form>
   )
